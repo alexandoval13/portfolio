6394787d9e439fa4a8585bc00dc4bd51f8a0df8e
Alexandra Sandoval <alexandoval13@gmail.com>
2024-11-18 13:47:02 -0800

    Update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* these config options export app as a static site and ensure asset paths work correctly on GitHub Pages */
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
 };
 
 export default nextConfig;
