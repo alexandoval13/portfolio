6e7c7ab9fb4c2b1ac825db2cd8683508fd1a31ca
Alexandra Sandoval <alexandoval13@gmail.com>
2024-11-19 10:38:43 -0800

    Use font from woff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import { Metadata } from 'next';
-import { Lobster } from 'next/font/google';
+// import { Lobster } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const lobster = localFont({
+  src: './fonts/Lobster.woff',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
   title: 'Alexandra Sandoval | Full-Stack Software Engineer',
   description: 'Portfolio',
 };
 
-const lobster = Lobster({ weight: '400', preload: false });
+// const lobster = Lobster({ weight: '400', preload: false });
 
 export default function RootLayout({
   children,
