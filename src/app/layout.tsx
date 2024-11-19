9862cac1f89e3d0b4e3fd53e7731ecf0803e7732
Alexandra Sandoval <alexandoval13@gmail.com>
2024-11-18 16:22:47 -0800

    Removes Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
 import { Metadata } from 'next';
 import { ThemeProvider } from '@mui/material/styles';
-import { Roboto, Lobster } from 'next/font/google';
+import { Lobster } from 'next/font/google';
 import theme from '../theme';
 
 export const metadata: Metadata = {
   title: 'Alexandra Sandoval | Full-Stack Software Engineer',
   description: 'Portfolio',
 };
-
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto',
-});
 
 const lobster = Lobster({ weight: '400', preload: false });
 
--- src/theme.ts
+++ src/theme.ts
@@ -4,9 +4,6 @@ import { createTheme } from '@mui/material';
 
 const theme = createTheme({
   cssVariables: true,
-  typography: {
-    fontFamily: 'var(--font-roboto)',
-  },
 });
 
 export default theme;
